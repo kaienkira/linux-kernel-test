b744dfb589c96d2f443eed5e1a4af6e7188f5e2a
kaien <kaienkira@gmail.com>
2023-08-22 11:37:37 +0800

    work sync

--- tools/download_source.sh
+++ tools/download_source.sh
@@ -27,16 +27,16 @@ download_file()
     fi
 }
 
-download_file 'https://git.kernel.org/torvalds/t/linux-6.3-rc2.tar.gz'
-download_file 'http://ftp.gnu.org/gnu/glibc/glibc-2.37.tar.xz'
-download_file 'https://busybox.net/downloads/busybox-1.36.0.tar.bz2'
+download_file 'https://git.kernel.org/torvalds/t/linux-6.5-rc7.tar.gz'
+download_file 'http://ftp.gnu.org/gnu/glibc/glibc-2.38.tar.xz'
+download_file 'https://busybox.net/downloads/busybox-1.36.1.tar.bz2'
 download_file 'https://www.netfilter.org/projects/libmnl/files/libmnl-1.0.5.tar.bz2'
-download_file 'https://www.netfilter.org/projects/libnftnl/files/libnftnl-1.2.4.tar.bz2'
+download_file 'https://www.netfilter.org/projects/libnftnl/files/libnftnl-1.2.6.tar.bz2'
 download_file 'https://www.netfilter.org/projects/libnfnetlink/files/libnfnetlink-1.0.2.tar.bz2'
 download_file 'https://www.netfilter.org/projects/libnetfilter_conntrack/files/libnetfilter_conntrack-1.0.9.tar.bz2'
-download_file 'https://www.netfilter.org/projects/nftables/files/nftables-1.0.6.tar.xz'
-download_file 'https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-6.1.0.tar.xz'
-download_file 'https://github.com/esnet/iperf/archive/refs/tags/3.11.tar.gz' 'iperf-3.11.tar.gz'
+download_file 'https://www.netfilter.org/projects/nftables/files/nftables-1.0.8.tar.xz'
+download_file 'https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-6.4.0.tar.xz'
+download_file 'https://github.com/esnet/iperf/archive/refs/tags/3.14.tar.gz' 'iperf-3.14.tar.gz'
 download_file 'https://matt.ucc.asn.au/dropbear/releases/dropbear-2022.83.tar.bz2'
 download_file 'https://ftp.gnu.org/gnu/grub/grub-2.06.tar.xz'
 
